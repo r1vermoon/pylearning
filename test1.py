c2f6e2ed787a4edddfd822d36b62540b8e7358a4
JJYJJY12138 <24151213794@stu.xidian.edu.cn>
2024-11-14 20:32:47 +0800

    chapter2

---
+++ test1.py
@@ -0,0 +1,39 @@
+message="Hello World!"
+print(message)
+
+message="Hello Python World!"
+print(message)
+
+name="ada lovelace"
+print(name.title())
+
+name="Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+first_name="ada"
+last_name="lovelace"
+full_name=first_name+' '+last_name
+print(full_name)
+
+print("hello,"+full_name.title()+'!')
+
+message="hello,"+full_name.title()+'!'
+print(message)
+
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favourite_language=' python '
+favourite_language=favourite_language.strip()
+print(favourite_language)
+
+myname="     r1vermoon   "
+print(myname.strip())
+
+print(10**6)
+
+age=23
+message="Happy "+str(age)+"rd Birthday!"
+print(message)
