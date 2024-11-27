cea2277fa89ce0006f16d4b2a4a4d7eac12fe9b4
JJYJJY12138 <24151213794@stu.xidian.edu.cn>
2024-11-27 18:06:03 +0800

    chap6

---
+++ test5.py
@@ -0,0 +1,45 @@
+#字典
+alien_0={'color':'green','points':5}
+print(alien_0['color'])
+
+# 添加
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+#修改
+alien_0['color'] ='yellow'
+print(alien_0)
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+
+if alien_0['speed'] == 'slow': 
+    x_increment = 1 
+elif alien_0['speed'] == 'medium': 
+    x_increment = 2 
+else: 
+    x_increment = 3  
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(alien_0['x_position'])
+
+#删除
+del alien_0['speed']
+print(alien_0)
+
+#遍历
+user_0 = { 'username': 'efermi', 'first': 'enrico', 'last': 'fermi', }
+for key,value in user_0.items():
+    print("\nkey: "+ key)
+    print("\nvalue: "+ value)
+
+for key in user_0.keys(): 
+    print(key.title())
+
+alien_0 = {'color': 'green', 'points': 5} 
+alien_1 = {'color': 'yellow', 'points': 10} 
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens: print(alien)
